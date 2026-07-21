d040238eb2fee64f605170e7836e69e9c52ca474
Bastian <bastian-2404@hotmail.com>
2026-07-20 22:13:41 -0400

    ceate user

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api/v1');
+  // Enable global validation pipe for request DTOs
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // Enable global Prisma client exception filter
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  const port = process.env.PORT ?? 3000;
+  console.log(`Server is running on port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
